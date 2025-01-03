2680e984099d58967cc1cdb9471c42d1b5571b41
who <pokemonbolle@gmail.com>
2025-01-03 23:28:34 +0100

    add debugger

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -4,6 +4,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 --------- keymapping ----------
 vim.g.mapleader = " "
@@ -11,3 +13,4 @@ vim.api.nvim_set_keymap('n', 'j', 'k', { noremap = true })
 vim.api.nvim_set_keymap('n', 'k', 'j', { noremap = true })
 vim.api.nvim_set_keymap('v', 'j', 'k', { noremap = true })
 vim.api.nvim_set_keymap('v', 'k', 'j', { noremap = true })
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', {desc = 'Yank to clipboard'})
