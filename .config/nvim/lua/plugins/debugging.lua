911f2f815194b672f8af20d33edab640b67fa94d
who <pokemonbolle@gmail.com>
2025-01-03 23:45:07 +0100

    broken debugigng

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -2,7 +2,7 @@ return {
     "mfussenegger/nvim-dap",
     dependencies = {
         "rcarriga/nvim-dap-ui",
-        "nvim-neotest/nvim-nio"
+        "nvim-neotest/nvim-nio",
     },
     config = function()
         local dap = require("dap")
@@ -10,6 +10,47 @@ return {
 
         require("dapui").setup()
         -- require("somelanguagedebugger").setup()
+        dap.adapters.gdb = {
+            type = "executable",
+            command = "gdb",
+            args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+        }
+
+        dap.configurations.c = {
+            {
+                name = "Launch",
+                type = "gdb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+                stopAtBeginningOfMainSubprogram = false,
+            },
+            {
+                name = "Select and attach to process",
+                type = "gdb",
+                request = "attach",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                pid = function()
+                    local name = vim.fn.input("Executable name (filter): ")
+                    return require("dap.utils").pick_process({ filter = name })
+                end,
+                cwd = "${workspaceFolder}",
+            },
+            {
+                name = "Attach to gdbserver :1234",
+                type = "gdb",
+                request = "attach",
+                target = "localhost:1234",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+            },
+        }
 
         dap.listeners.before.attach.dapui_config = function()
             dapui.open()
@@ -24,7 +65,7 @@ return {
             dapui.close()
         end
 
-        vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, {})
-        vim.keymap.set('n', '<leader>dc', dap.continue, {})
-    end
+        vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+        vim.keymap.set("n", "<leader>dc", dap.continue, {})
+    end,
 }
