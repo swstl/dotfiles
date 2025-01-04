db972cda49e042b22f7a8317e38fc5b060b8aa1e
who <pokemonbolle@gmail.com>
2025-01-04 14:29:32 +0100

    add whichkey

---
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,17 @@
+return {
+    "folke/which-key.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    opts = {
+        delay = 0,
+    },
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = true })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
+}
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -14,3 +14,6 @@ vim.api.nvim_set_keymap('n', 'k', 'j', { noremap = true })
 vim.api.nvim_set_keymap('v', 'j', 'k', { noremap = true })
 vim.api.nvim_set_keymap('v', 'k', 'j', { noremap = true })
 vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', {desc = 'Yank to clipboard'})
+vim.keymap.set('n', '<C-c>', 'gcc', { remap = true, desc = 'Toggle comment' })
+vim.keymap.set('v', '<C-c>', 'gc', { remap = true, desc = 'Toggle comment' })
+
