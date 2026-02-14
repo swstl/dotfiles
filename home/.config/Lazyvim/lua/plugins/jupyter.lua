a1977464c77c2cd98cf67cda17776346ad409ac6
swstl <sander_we03@hotmail.com>
2026-02-14 22:12:41 +0100

    change jupyter plugin

--- home/.config/Lazyvim/lua/plugins/jupyter.lua
+++ home/.config/Lazyvim/lua/plugins/jupyter.lua
@@ -39,20 +39,7 @@ return {
       },
       -- Iron doesn't set keymaps by default anymore.
       -- You can set them here or manually add keymaps to the functions in iron.core
-      keymaps = {
-        send_motion = "<space>rc",
-        visual_send = "<space>rc",
-        send_file = "<space>rf",
-        send_line = "<space>rl",
-        send_mark = "<space>rm",
-        mark_motion = "<space>rmc",
-        mark_visual = "<space>rmc",
-        remove_mark = "<space>rmd",
-        cr = "<space>r<cr>",
-        interrupt = "<space>r<space>",
-        exit = "<space>rq",
-        clear = "<space>rx",
-      },
+      keymaps = {},
       -- If the highlight is on, you can change how it looks
       -- For the available options, check nvim_set_hl
       highlight = {
@@ -61,26 +48,63 @@ return {
       ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
     })
 
+    -- Iron keymaps only for regular .py files (not .ju.py)
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "python",
       callback = function()
-        -- Normal mode: Run `iron.send_line()` (send current line)
-        vim.api.nvim_buf_set_keymap(
-          0,
-          "n",
-          "<CR>",
-          "<cmd>lua require('iron.core').send_line()<CR>",
-          { noremap = true, silent = true }
-        )
+        local bufname = vim.api.nvim_buf_get_name(0)
+        if bufname:match("%.ju%.py$") then
+          return
+        end
 
-        -- Visual mode: Run `iron.visual_send()` (send selected block)
-        vim.api.nvim_buf_set_keymap(
-          0,
-          "v",
-          "<CR>",
-          "<cmd>lua require('iron.core').visual_send()<CR>",
-          { noremap = true, silent = true }
-        )
+        local map = function(mode, lhs, rhs)
+          vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, { noremap = true, silent = true })
+        end
+
+        -- Iron REPL keymaps
+        map("n", "<space>rc", "<cmd>lua require('iron.core').send_motion()<CR>")
+        map("v", "<space>rc", "<cmd>lua require('iron.core').visual_send()<CR>")
+        map("n", "<space>rf", "<cmd>lua require('iron.core').send_file()<CR>")
+        map("n", "<space>rl", "<cmd>lua require('iron.core').send_line()<CR>")
+        map("n", "<space>rm", "<cmd>lua require('iron.core').send_mark()<CR>")
+        map("n", "<space>rmc", "<cmd>lua require('iron.core').mark_motion()<CR>")
+        map("v", "<space>rmc", "<cmd>lua require('iron.core').mark_visual()<CR>")
+        map("n", "<space>rmd", "<cmd>lua require('iron.core').remove_mark()<CR>")
+        map("n", "<space>r<cr>", "<cmd>lua require('iron.core').cr()<CR>")
+        map("n", "<space>r<space>", "<cmd>lua require('iron.core').interrupt()<CR>")
+        map("n", "<space>rq", "<cmd>lua require('iron.core').exit()<CR>")
+        map("n", "<space>rx", "<cmd>lua require('iron.core').clear()<CR>")
+        map("n", "<CR>", "<cmd>lua require('iron.core').send_line()<CR>")
+        map("v", "<CR>", "<cmd>lua require('iron.core').visual_send()<CR>")
+      end,
+    })
+
+    -- Jupynium keymaps only for .ju.py files
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "python",
+      callback = function()
+        local bufname = vim.api.nvim_buf_get_name(0)
+        if not bufname:match("%.ju%.py$") then
+          return
+        end
+
+        local map = function(mode, lhs, rhs)
+          vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, { noremap = true, silent = true })
+        end
+
+        -- Jupynium keymaps
+        map("n", "<space>rx", "<cmd>JupyniumExecuteSelectedCells<CR>")
+        map("n", "<space>rc", "<cmd>JupyniumClearSelectedCellsOutputs<CR>")
+        map("n", "<space>rK", "<cmd>JupyniumKernelHover<CR>")
+        map("n", "<space>rR", "<cmd>JupyniumKernelRestart<CR>")
+        map("n", "<space>ri", "<cmd>JupyniumKernelInterrupt<CR>")
+        map("n", "<space>rs", "<cmd>JupyniumStartSync<CR>")
+        map("n", "<space>rS", "<cmd>JupyniumStopSync<CR>")
+        map("n", "<space>ra", "<cmd>JupyniumStartAndAttachToServer<CR>")
+        map("n", "<space>rk", "<cmd>JupyniumKernelSelect<CR>")
+        map("n", "<space>ro", "<cmd>JupyniumToggleSelectedCellsOutputsScroll<CR>")
+        map("n", "<space>rd", "<cmd>JupyniumDownloadIpynb<CR>")
+        map("n", "<CR>", "<cmd>JupyniumExecuteSelectedCells<CR>")
       end,
     })
   end,
@@ -93,6 +117,7 @@ return {
     -- build = "conda run --no-capture-output -n jupynium pip install .",
     opts = {
       python_host = vim.fn.expand "~/.virtualenvs/jupynium/bin/python",
+      jupyter_command = vim.fn.expand "~/.virtualenvs/jupynium/bin/jupyter",
     },
   },
   -- "rcarriga/nvim-notify", -- optional
