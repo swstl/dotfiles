6c931ac19874688076f9568b26db300cdcb7e6fb
swstl <sander_we03@hotmail.com>
2026-03-07 14:37:07 +0100

    avante copilot

--- home/.config/Lazyvim/lua/plugins/surround.lua
+++ home/.config/Lazyvim/lua/plugins/surround.lua
@@ -3,19 +3,21 @@ return {
     "kylechui/nvim-surround",
     version = "*",
     event = "VeryLazy",
+    init = function()
+      vim.g.nvim_surround_no_mappings = true
+    end,
     config = function()
-      require("nvim-surround").setup({
-        keymaps = {
-          normal = "gsa", -- Add surrounding in Normal mode
-          visual = "gsa", -- Add surrounding in Visual mode
-          delete = "gsd", -- Delete surrounding
-          find = "gsf", -- Find surrounding (to the right)
-          find_left = "gsF", -- Find surrounding (to the left)
-          highlight = "gsh", -- Highlight surrounding
-          replace = "gsr", -- Replace surrounding
-          update_n_lines = "gsn", -- Update `n_lines`
-        },
-      })
+      require("nvim-surround").setup()
+      vim.keymap.set("n", "gsa", "<Plug>(nvim-surround-normal)", { desc = "Add surrounding (normal)" })
+      vim.keymap.set("n", "gsA", "<Plug>(nvim-surround-normal-line)", { desc = "Add surrounding (normal, line)" })
+      vim.keymap.set("x", "gsa", "<Plug>(nvim-surround-visual)", { desc = "Add surrounding (visual)" })
+      vim.keymap.set("x", "gsA", "<Plug>(nvim-surround-visual-line)", { desc = "Add surrounding (visual, line)" })
+      vim.keymap.set("n", "gsd", "<Plug>(nvim-surround-delete)", { desc = "Delete surrounding" })
+      vim.keymap.set("n", "gsf", "<Plug>(nvim-surround-find)", { desc = "Find surrounding (right)" })
+      vim.keymap.set("n", "gsF", "<Plug>(nvim-surround-find-left)", { desc = "Find surrounding (left)" })
+      vim.keymap.set("n", "gsh", "<Plug>(nvim-surround-highlight)", { desc = "Highlight surrounding" })
+      vim.keymap.set("n", "gsr", "<Plug>(nvim-surround-change)", { desc = "Replace surrounding" })
+      vim.keymap.set("n", "gsn", "<Plug>(nvim-surround-change-line)", { desc = "Replace surrounding (line)" })
     end,
   },
 }
